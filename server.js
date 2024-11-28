f03f985c4d7b6a4ac1e5ada466f5d86ffa299d68
Canelle-Bodin <canelle.bodin@serli.com>
2024-11-28 12:13:30 +0100

    fix deploy 13

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Sert les fichiers statiques de l'application Angular
-app.use(express.static(path.join(__dirname, 'dist/eclessence/browser')));
+app.use(express.static(path.join(__dirname, 'dist/eclessence')));
 
 // Redirige toute requête vers l'index.html de l'application Angular
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/eclessence/browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/eclessence', 'index.html'));
 });
 
 // Le serveur écoute sur le port configuré par Clever Cloud ou 8080 par défaut
